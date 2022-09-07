31f97e42515ae6dc509250195e6f0e3d4cc2e192
cst <cst@nilu.no>
2022-09-07 15:25:14 +0200

    dev: center bottom text

--- client/src/views/notfound/Notfound.vue
+++ client/src/views/notfound/Notfound.vue
@@ -5,7 +5,7 @@
     <div class="flex flex-col items-center gap-2">
       <h1 class="font-bold text-9xl">404</h1>
       <h2 class="font-bold text-xl">Page Not Found</h2>
-      <label class="text-lg">The page you are looking for does not exists or another error occured</label>
+      <label class="text-lg text-center">The page you are looking for does not exists or another error occured</label>
     </div>
   </div>
 </template>
