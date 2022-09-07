d5e41f16c99e90b01cd5f2f5e17bf469ca0acd09
cst <cst@nilu.no>
2022-09-07 15:27:11 +0200

    bug: wrong url was used

--- client/src/views/qualitycontrol/validate/service.js
+++ client/src/views/qualitycontrol/validate/service.js
@@ -1,7 +1,7 @@
 import { Get, Post } from "../../../helpers/request";
 
 const Service = {
-  get: async (data) => Post("/api/qualitycontrol/validate", data),
+  get: async (data) => Post("/api/qualitycontrol/validate/timevalues", data),
   timeseries: async () => Get("/api/qualitycontrol/validate/timeseries"),
   validate: async (data) => Post("/api/qualitycontrol/validate/flag", data)
 };
