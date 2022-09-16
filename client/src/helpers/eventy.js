e62bf80075f02b39fd2a6d9dc5871928838d5318
cst <cst@nilu.no>
2022-09-16 10:18:40 +0200

    feat: let failed progressbar last longer

--- client/src/helpers/eventy.js
+++ client/src/helpers/eventy.js
@@ -1,33 +1,32 @@
-import mitt from 'mitt';
+import mitt from "mitt";
 const emitter = mitt();
 
 const Eventy = {
-    showProgress: () => {
-        emitter.emit('showProgress');
-    },
-    hideProgress: (ms = 500) => {
-        setTimeout(() => emitter.emit('hideProgress'), ms);
-    },
-    failProgress: (s) => {
-        emitter.emit('failProgress', s);
-        Eventy.hideProgress(1000)
-    },
-    showMessage: (msg, type) => {
-        emitter.emit('showMessage', { msg, type });
-    },
-    hideMessage: (ms = 0) => {
-        setTimeout(() => emitter.emit('hideMessage'), ms);
-    },
-    showHideMessage: (msg, type, duration = 5000) => {
-        emitter.emit('showMessage', { msg, type });
-        Eventy.hideMessage(duration);
-    },
-    listen: (e, f) => {
-        emitter.on(e, f);
-    },
-    emit: (e, f) => {
-        emitter.emit(e, f);
-    }
-
+  showProgress: () => {
+    emitter.emit("showProgress");
+  },
+  hideProgress: (ms = 500) => {
+    setTimeout(() => emitter.emit("hideProgress"), ms);
+  },
+  failProgress: (s) => {
+    emitter.emit("failProgress", s);
+    Eventy.hideProgress(2000);
+  },
+  showMessage: (msg, type) => {
+    emitter.emit("showMessage", { msg, type });
+  },
+  hideMessage: (ms = 0) => {
+    setTimeout(() => emitter.emit("hideMessage"), ms);
+  },
+  showHideMessage: (msg, type, duration = 5000) => {
+    emitter.emit("showMessage", { msg, type });
+    Eventy.hideMessage(duration);
+  },
+  listen: (e, f) => {
+    emitter.on(e, f);
+  },
+  emit: (e, f) => {
+    emitter.emit(e, f);
+  }
 };
 export default Eventy;
