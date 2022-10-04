4e3103a499d3084c7a4ad478bf868797ad81897c
Håvard Vika Røen <hvr@nilu.no>
2022-10-04 08:20:19 +0200

    columnpicker changed to contextmenu

--- client/src/components/ToolBar.vue
+++ client/src/components/ToolBar.vue
@@ -2,7 +2,6 @@
 import IconAdd from "~icons/ic/baseline-add";
 import IconDownload from "~icons/ic/round-file-download";
 import IconRemoveRedEye from "~icons/ic/baseline-remove-red-eye";
-import NCheckbox from "./n-elements/NCheckbox.vue";
 
 const props = defineProps({
   title: {
@@ -39,8 +38,14 @@ const props = defineProps({
   }
 });
 
+const ev = ref({});
 const showColumnPicker = ref(false);
 const emit = defineEmits(["update:modelValue", "add-click", "download-click"]);
+
+const onContextMenu = (e) => {
+  ev.value = e;
+  showColumnPicker.value = !showColumnPicker.value;
+};
 </script>
 
 <template>
@@ -58,20 +63,26 @@ const emit = defineEmits(["update:modelValue", "add-click", "download-click"]);
       <div v-if="showFilter" class="flex">
         <input :placeholder="filterText" class="n-input" type="search" :value="modelValue" input="$emit('update:modelValue', $event.target.value)" v-bind="$attrs" />
         <div v-if="columnPicker" class="flex">
-          <circle-hover class="ml-1 self-center" @click="showColumnPicker = !showColumnPicker" v-if="showAdd">
+          <circle-hover class="ml-1 self-center" @click="onContextMenu($event)" v-if="showAdd" @contextmenu.prevent="onContextMenu($event)">
             <icon-remove-red-eye class="text-nord14 text-lg self-center" />
           </circle-hover>
         </div>
       </div>
     </div>
-    <div class="p-3 mb-1 bg-gray-100 border-2" v-if="showColumnPicker">
-      <div v-for="(column, key) in columnPicker">
+    <contextmenu :evt="ev" :show="showColumnPicker" @click-outside="showColumnPicker = false">
+      <div v-for="(column, key) in columnPicker" class="pl-2 pr-4 py-2 flex cursor-pointer hover:bg-gray-100">
         <input class="n-checkbox" type="checkbox" :id="key" :value="column" @input="$emit('columns-changed', key)" :checked="columnsPicked?.includes(key)" />
         <label :for="key">
           {{ column }}
         </label>
       </div>
-    </div>
+    </contextmenu>
+    <!-- <div v-for="(column, key) in columnPicker">
+        <input class="n-checkbox" type="checkbox" :id="key" :value="column" @input="$emit('columns-changed', key)" :checked="columnsPicked?.includes(key)" />
+        <label :for="key">
+          {{ column }}
+        </label>
+      </div> -->
   </div>
 </template>
 
