61578c69f5cebf4a9e604c04309404e8bd300ac8
cst <cst@nilu.no>
2022-11-10 10:39:12 +0100

    refactor: removed unused code

--- client/src/components/ColumnPicker.vue
+++ client/src/components/ColumnPicker.vue
@@ -14,10 +14,6 @@ const props = defineProps({
         <td class="!px-3 !p-1 self-center flex"><n-checkbox v-model="p.showInGrid"></n-checkbox></td>
       </tr>
     </table>
-    <!-- <div v-for="p in properties" class="pl-4 pr-4 py-1 flex cursor-pointer gap-8 hover:bg-gray-100">
-      <div class="flex-1" @click="p.showInGrid = !p.showInGrid">{{ p.label }}</div>
-      <n-checkbox class="self-center" v-model="p.showInGrid"></n-checkbox>
-    </div> -->
   </contextmenu>
 </template>
 
