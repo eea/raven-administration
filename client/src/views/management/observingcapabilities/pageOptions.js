aa60261d1efb45f01d732b6b909ca9e99e3f0d55
cst <cst@nilu.no>
2022-11-10 14:45:01 +0100

    refactor: rearranged the order of columns in grid

--- client/src/views/management/observingcapabilities/pageOptions.js
+++ client/src/views/management/observingcapabilities/pageOptions.js
@@ -4,12 +4,12 @@ const pageOptions = (lookups) => ({
     // REQUIRED
     { type: "text", label: "Id", prop: "id", placeholder: "str: A unique id", required: true, default: null, showInEdit: false, showInGrid: true },
     { type: "lookup", label: "Sampling point", prop_id: "sampling_point_id", prop: "sampling_point_id", lookup: "sampling_points", required: true, default: null, showInEdit: true, showInGrid: true },
-    { type: "text", label: "Begin", prop: "begin_position", placeholder: "str: YYYY-MM-DDTHH:mm:ssZ", required: true, default: null, showInEdit: true, showInGrid: true },
 
     { type: "lookup", label: "Process", prop_id: "process_id", prop: "process_id", required: true, lookup: "processes", default: null, showInEdit: true, showInGrid: true },
     { type: "lookup", label: "Sample", prop_id: "sample_id", prop: "sample_id", required: true, lookup: "samples", default: null, showInEdit: true, showInGrid: true },
     { type: "lookup", label: "Result nature", prop_id: "result_nature_id", prop: "result_nature", required: true, lookup: "result_nature_values", default: null, showInEdit: true, showInGrid: false },
     { type: "lookup", label: "Process type", prop_id: "process_type_id", prop: "process_type", required: true, lookup: "processtype_values", default: null, showInEdit: true, showInGrid: false },
+    { type: "text", label: "Begin", prop: "begin_position", placeholder: "str: YYYY-MM-DDTHH:mm:ssZ", required: true, default: null, showInEdit: true, showInGrid: true },
 
     // OPTIONAL
     { type: "text", label: "End", prop: "end_position", placeholder: "str: YYYY-MM-DDTHH:mm:ssZ", required: false, default: null, showInEdit: true, showInGrid: false }
