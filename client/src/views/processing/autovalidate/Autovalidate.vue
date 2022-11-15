82277166adfbffb1ef507156491b50d764875f9d
cst <cst@nilu.no>
2022-11-15 23:11:40 +0100

    refactor: simplify code by reusing

--- client/src/views/processing/autovalidate/Autovalidate.vue
+++ client/src/views/processing/autovalidate/Autovalidate.vue
@@ -1,117 +1,14 @@
 <script setup>
-import LAdd from "./LAdd.vue";
-import LEdit from "./LEdit.vue";
-
 import Service from "./service";
-import Eventy from "../../../helpers/eventy";
-import { tblToCsv, compare, filterList } from "../../../helpers/utils";
+import pageOptions from "./pageOptions";
 
-const pollutants = ref([]);
-const autovalidations = ref([]);
-const q = ref("");
-const showEdit = ref(false);
-const showAdd = ref(false);
-const selected = ref({});
-const ev = ref({});
-const showContextmenu = ref(false);
-const showConfirm = ref(false);
+const options = ref({});
 
 onMounted(async () => {
-  await loadData();
+  const pollutants = await Service.pollutants();
+  options.value = pageOptions({ pollutants });
 });
-
-const loadData = async () => {
-  pollutants.value = await Service.pollutants();
-  autovalidations.value = await Service.get();
-};
-
-const cmp_autovalidations = computed(() => filterList(q.value, autovalidations.value));
-
-const cls_rowClass = (row) => {
-  if (compare(selected.value, row)) return " selected";
-  return "";
-};
-
-const onContextMenu = (row, e) => {
-  selected.value = row;
-  ev.value = e;
-  showContextmenu.value = true;
-};
-
-const onEdit = () => {
-  if (showEdit.value) selected.value = {};
-  showEdit.value = !showEdit.value;
-  showContextmenu.value = false;
-};
-
-const onSaveEdit = async (o) => {
-  await Service.update(o);
-  await loadData();
-  Eventy.showHideMessage("Validation saved", "success", 5000);
-  close();
-};
-
-const onSaveAdd = async (o) => {
-  await Service.insert(o);
-  await loadData();
-  Eventy.showHideMessage("Validation saved", "success", 5000);
-  close();
-};
-
-const close = () => {
-  showEdit.value = false;
-  showAdd.value = false;
-  selected.value = {};
-  showContextmenu.value = false;
-  showConfirm.value = false;
-};
-
-const onDelete = () => {
-  if (showConfirm.value) selected.value = {};
-  showConfirm.value = !showConfirm.value;
-  showContextmenu.value = false;
-};
-
-const onSaveDelete = async (o) => {
-  showConfirm.value = false;
-  await Service.delete(selected.value);
-  await loadData();
-  Eventy.showHideMessage("Validation deleted", "success", 5000);
-  close();
-};
-
-const onDownload = () => {
-  tblToCsv("autovalidationsId", "autovalidations");
-};
 </script>
-
 <template>
-  <common-layout>
-    <confirm :show="showConfirm" title="Delete" text="Are you sure you want to delete validation?" @close="close" @ok="onSaveDelete" />
-    <contextmenu-crud :show="showContextmenu" :ev="ev" @click-outside="close" @on-edit="onEdit" @onDelete="onDelete" />
-
-    <l-add :show="showAdd" @close="close" @save="onSaveAdd" :pollutants="pollutants" />
-    <l-edit :show="showEdit" @close="close" @save="onSaveEdit" :pollutants="pollutants" :validation="selected" />
-
-    <tool-bar title="Auto validate" :show-column-picker="false" v-model:q="q" @add-click="showAdd = true" @download-click="onDownload" />
-
-    <div>
-      <table id="autovalidationsId" class="n-table">
-        <tr>
-          <th>Pollutant</th>
-          <th>Minimum</th>
-          <th>Maximum</th>
-          <th>Repeat</th>
-        </tr>
-        <tr v-for="row in cmp_autovalidations" @contextmenu.prevent="onContextMenu(row, $event)" @click="selected = {}" :class="cls_rowClass(row)">
-          <td>{{ row.pollutant }}</td>
-          <td>{{ row.min }}</td>
-          <td>{{ row.max }}</td>
-          <td>{{ row.rep }}</td>
-        </tr>
-      </table>
-    </div>
-  </common-layout>
+  <manager name="Auto validate" :options="options" :service="Service" />
 </template>
-
-<style></style>
