5892d864b0b90ecd4a8147fc7aa46a2f9c9fa8d3
cst <cst@nilu.no>
2022-11-25 15:29:10 +0100

    refactor: remove secret key default value. force user to set it

--- config.py
+++ config.py
@@ -9,6 +9,6 @@ load_dotenv(os.path.join(basedir, '.env'))
 
 class Config(object):
     DB_URI = os.environ.get('DB_URI') or "postgresql://user:psw@localhost/db"
-    JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY') or "super-secret-key"
+    JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY')
     JWT_ACCESS_TOKEN_EXPIRES_SECONDS = int(os.environ.get('JWT_ACCESS_TOKEN_EXPIRES_SECONDS')) or 3600
     JWT_ACCESS_TOKEN_EXPIRES = timedelta(seconds=JWT_ACCESS_TOKEN_EXPIRES_SECONDS)
