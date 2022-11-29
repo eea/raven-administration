b7487dbf21a1659444ad087dd770b19b82b7aa5e
cst <cst@nilu.no>
2022-11-29 18:19:39 +0100

    refactor: align cell to the top

--- client/src/views/processing/import/Import.vue
+++ client/src/views/processing/import/Import.vue
@@ -23,33 +23,33 @@ const onUpload = async () => {
         <div class="text-sm">
           <table class="">
             <tr>
-              <td class="font-bold">sampling_point_id</td>
+              <td class="font-bold align-top">sampling_point_id</td>
               <td class="pl-4">
                 A valid reference to a
                 <a href="/management/samplingpoints">sampling_point_id</a>
               </td>
             </tr>
             <tr>
-              <td class="font-bold">begin_position</td>
+              <td class="font-bold align-top">begin_position</td>
               <td class="pl-4">ISO 8601 date standard with format: 2001-01-01T00:00:00+01:00</td>
             </tr>
             <tr>
-              <td class="font-bold">end_position</td>
+              <td class="font-bold align-top">end_position</td>
               <td class="pl-4">ISO 8601 date standard with format: 2001-01-01T00:00:00+01:00</td>
             </tr>
             <tr>
-              <td class="font-bold">value</td>
+              <td class="font-bold align-top">value</td>
               <td class="pl-4">The measured value</td>
             </tr>
             <tr>
-              <td class="font-bold">validation_flag</td>
+              <td class="font-bold align-top">validation_flag</td>
               <td class="pl-4">
                 An integer verification flag.
                 <a href="http://dd.eionet.europa.eu/vocabulary/aq/observationvalidity">Read more about validation levels here</a>
               </td>
             </tr>
             <tr>
-              <td class="font-bold">verification_flag</td>
+              <td class="font-bold align-top">verification_flag</td>
               <td class="pl-4">
                 An integer validation flag.
                 <a href="http://dd.eionet.europa.eu/vocabulary/aq/observationverification">Read more about verification levels here</a>
