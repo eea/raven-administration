845710e57c2eaad6659ca59d4876be6fb13b1497
cst <cst@nilu.no>
2022-12-01 13:04:03 +0100

    feat: added sort to management columns

--- client/src/components/n-manager/Grid.vue
+++ client/src/components/n-manager/Grid.vue
@@ -1,5 +1,6 @@
 <script setup>
 import { compare } from "../../helpers/utils";
+import IconSort from "~icons/mdi/sort-alphabetical-ascending";
 
 const props = defineProps({
   id: String,
@@ -8,6 +9,8 @@ const props = defineProps({
   selected: Object,
   ev: Object
 });
+
+const sort_header = ref();
 
 const emit = defineEmits(["update:selected", "update:ev", "on-right-click"]);
 
@@ -36,15 +39,32 @@ const onClick = () => {
   emit("update:selected", {});
   emit("update:ev", {});
 };
+
+const cmp_sorted = computed(() => {
+  if (!sort_header.value) return props.values;
+  try {
+    var s = Object.assign(
+      [],
+      props.values.map((o) => Object.assign({}, o))
+    );
+    return s.sort((x, y) => x[sort_header.value].localeCompare(y[sort_header.value]));
+  } catch (error) {
+    sort_header.value = undefined;
+    return props.values;
+  }
+});
 </script>
 
 <template>
   <div>
     <table :id="id" class="n-table">
       <tr>
-        <th v-for="header in properties" v-show="header.showInGrid">{{ header.label }}</th>
+        <th v-for="header in properties" v-show="header.showInGrid">
+          <span @click="sort_header = sort_header == header.prop ? undefined : header.prop" class="cursor-pointer">{{ header.label }}</span>
+          <icon-sort class="inline ml-1 text-nord10" v-show="sort_header == header.prop" />
+        </th>
       </tr>
-      <tr v-for="row in values" :class="cls_rowClass(row)" @contextmenu.prevent="onRightClick(row, $event)" @click="onClick()">
+      <tr v-for="row in cmp_sorted" :class="cls_rowClass(row)" @contextmenu.prevent="onRightClick(row, $event)" @click="onClick()">
         <td v-for="header in properties" v-show="header.showInGrid" :class="cls_cellClass(header, row)">
           <n-checkbox v-if="header.type == 'checkbox'" class="align-middle" v-model="row[header.prop]" :disabled="true" />
           <span v-else-if="header.type == 'gridOnly'">{{ fn_cellVal(header, row) }}</span>
