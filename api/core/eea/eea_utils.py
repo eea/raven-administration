2ba9501df7c71afce692c4b6169472d461fb93ae
store <store>
2023-10-02 14:31:52 +0200

    bug: Correction needed because "%z" gives you timezone offset without colon.

--- api/core/eea/eea_utils.py
+++ api/core/eea/eea_utils.py
@@ -6,4 +6,7 @@ class EeaUtils:
     @staticmethod
     def local_datetime():
         now = datetime.now(tz=tzlocal())  # current date and time
-        return now.strftime("%Y-%m-%dT%H:%M:%S%z")
+        now_str = now.strftime("%Y-%m-%dT%H:%M:%S%z")
+        # Correction needed because "%z" gives you timezone offset without colon.
+        now_str_corr = "{0}:{1}".format(now_str[:-2], now_str[-2:])
+        return now_str_corr
