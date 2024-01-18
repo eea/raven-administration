5cdb89c3395fc5de0a5ccab956fad12ac2cccdb8
cst <cst@nilu.no>
2024-01-18 11:36:31 +0100

    docker and config changes

--- api/config.py
+++ api/config.py
@@ -4,7 +4,7 @@ from datetime import datetime
 from datetime import timedelta
 
 basedir = os.path.abspath(os.path.dirname(__file__))
-load_dotenv(os.path.join(basedir, '.env'))
+load_dotenv(os.path.join(basedir, '../.env'))
 
 
 class Config(object):
