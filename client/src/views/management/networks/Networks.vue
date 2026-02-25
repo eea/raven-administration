95ecc80b4994f8731bbb0ac3a8e7d104c5ca54b4
cst <cst@nilu.no>
2026-02-25 12:50:13 +0100

    update networks for new db structure

--- client/src/views/management/networks/Networks.vue
+++ client/src/views/management/networks/Networks.vue
@@ -3,17 +3,12 @@ import { onMounted, ref } from "vue";
 import Manager from "../../../components/n-manager/Manager.vue";
 import Service from "./service";
 import pageOptions from "./pageOptions";
-import Eventy from "../../../helpers/eventy";
+
 const options = ref({});
 
 onMounted(async () => {
-  Eventy.showMessage("Loading metadata", "loading");
-  const authorities = await Service.authorities();
-  const levels = await Service.levels();
-  const media = await Service.media();
-  const timezones = await Service.timezones();
-  Eventy.hideMessage();
-  options.value = pageOptions({ authorities, levels, media, timezones });
+  const lookups = await Service.lookups();
+  options.value = pageOptions(lookups);
 });
 </script>
 <template>
