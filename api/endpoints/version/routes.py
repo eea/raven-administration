dc49fc4f9798c9519443bf87a505ae77d3518463
cst <cst@nilu.no>
2026-02-26 19:11:43 +0100

    bump to beta12

--- api/endpoints/version/routes.py
+++ api/endpoints/version/routes.py
@@ -3,7 +3,7 @@ from flask_jwt_extended import create_access_token
 import requests
 
 version_endpoint = Blueprint('version', __name__)
-current_version = "4.0.0-beta11"
+current_version = "4.0.0-beta12"
 
 
 @version_endpoint.route('/api/version', methods=['GET'])
