079225a00408b084dcf305b6281643f31b714924
cst <cst@nilu.no>
2026-03-03 10:09:01 +0100

    removed old endpoints

--- api/endpoints/data/dataflow/routes.py
+++ api/endpoints/data/dataflow/routes.py
@@ -1,17 +1,11 @@
-from flask import jsonify, Blueprint, request, current_app, Response
-from flask_jwt_extended import jwt_required
+from flask import jsonify, Blueprint, request, Response
 from werkzeug.exceptions import BadRequest
-from core.query import Q
 from core.utils import U
 from core.database import CursorFromPool
-from endpoints.data.dataflow.models import DataflowModel, DataflowModelE2a
-from core.eea.dataflows import Dataflows
-from core.eea.dataflows_reportnet3 import Dataflows_reportnet3
 from core.jwt_ext_custom import jwt_required_with_exporting_claim
 from core.data import dataflow as DataflowExports
 import zipfile
 from io import BytesIO
-import pandas as pd
 from datetime import datetime
 
 dataflow_endpoint = Blueprint('dataflow', __name__)
@@ -192,68 +186,3 @@ def export_all_csv():
     return response
 
 
-# OLD DATAFLOW ENDPOINTS (XML based - kept for backward compatibility)
-
-@dataflow_endpoint.route('/api/dataflow', methods=['GET'])
-@jwt_required_with_exporting_claim()
-def dataflows():
-    m = DataflowModel(**request.args.to_dict())
-    xml = Dataflows.get_xml(m.type, m.year, m.timezone, m.description)
-    if xml == None:
-        raise BadRequest("Could not create xml for dataflow " + m.type)
-
-    return U.xmlify(xml)
-
-
-@dataflow_endpoint.route('/api/dataflow/e2a', methods=['GET'])
-@jwt_required_with_exporting_claim()
-def dataflows_e2a():
-    m = DataflowModelE2a(**request.args.to_dict())
-    xml = Dataflows.get_dataflowE2A(m.last_request)
-    if xml == None:
-        raise BadRequest("Could not create xml for dataflow E2a")
-
-    return U.xmlify(xml)
-
-
-# OLD REPORTNET3 ENDPOINTS - DEPRECATED
-# These endpoints have been replaced by the new CSV export system above
-# Kept commented for reference, can be removed after migration is complete
-
-# @dataflow_endpoint.route('/api/dataflow/reportnet3/csv', methods=['GET'])
-# @jwt_required_with_exporting_claim()
-# def dataflows_reportnet3():
-#     m = DataflowModel(**request.args.to_dict())
-#     data = Dataflows_reportnet3.get_dataflow(m.type, m.year, m.timezone, m.description)
-#     if not data:
-#         raise BadRequest("Could not create csv for dataflow "+m.type+", no data found")
-#     # ... rest of implementation
-
-# @dataflow_endpoint.route('/api/dataflow/showreportnet3', methods=['GET'])
-# @jwt_required()
-# def show_reportnet3():
-#     return jsonify({"showreportnet3": True})
-
-# @dataflow_endpoint.route('/api/dataflow/reportnet3/b', methods=['GET'])
-# @jwt_required_with_exporting_claim()
-# def dataflows_reportnet3_b():
-#     data = Dataflows_reportnet3.get_dataflowB()
-#     if not data:
-#         raise BadRequest("No data for dataflow B found")
-#     return jsonify(data)
-
-# @dataflow_endpoint.route('/api/dataflow/reportnet3/d/processes', methods=['GET'])
-# @jwt_required_with_exporting_claim()
-# def dataflows_reportnet3_d_processes():
-#     data = Dataflows_reportnet3.get_processes()
-#     if not data:
-#         raise BadRequest("No data for dataflow D Processes found")
-#     return jsonify(data)
-
-# @dataflow_endpoint.route('/api/dataflow/reportnet3/d/samplingpoints', methods=['GET'])
-# @jwt_required_with_exporting_claim()
-# def dataflows_reportnet3_d_samplingpoints():
-#     data = Dataflows_reportnet3.get_samplingpoints()
-#     if not data:
-#         raise BadRequest("No data for dataflow D Samplingpoints found")
-#     return jsonify(data)
